2100e285f49bc128419aa578895129d9cebe49c0
mohamed1253 <102216345+mohamed1253@users.noreply.github.com>
2022-09-16 00:54:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let deg = 50;
+if(deg == 100 && deg >= 90 ){
+    console.log(" YOU GOT  A");
+
+}
+if(deg <=  90  &&  deg >= 80){
+    console.log(" YOU GOT   B");
+
+}
+if(deg <=  80  &&  deg >= 70){
+    console.log(" YOU GOT   C");
+
+}
+if(deg <=  70  &&  deg >= 60){
+    console.log(" YOU GOT   D");
+
+}
+if(deg <= 60  &&  deg >= 40){
+    console.log(" YOU GOT   E");
+
+}
+if(deg <= 40 &&  deg >= 0){
+    console.log(" YOU GOT   F");
+
+}
